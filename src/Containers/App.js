3ec0cfe764ff442170454f4e3589f73aa61bb3b6
Marlin Blue <marlin.blue@sussexcountyde.gov>
2022-01-27 14:25:38 -0500

    Initial commit

---
+++ src/Containers/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import '../Sass/Styles.scss';
+import 'tachyons';
+import bgimage from '../Images/pattern-randomized.svg';
+
+function App() {
+  return (
+    <div className="App vh-100" style={{backgroundImage: `url(${bgimage})`}}>
+       
+    </div>
+  );
+}
+
+export default App;
