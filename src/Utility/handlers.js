5a8d5d8cd58c5b55a5b6a73a477a36004a02d953
Marlin <blutuu1@gmail.com>
2022-02-21 17:24:19 -0500

    Added basic file drop handling.

--- src/Utility/handlers.js
+++ src/Utility/handlers.js
@@ -29,6 +29,23 @@ export const dragLeaveHandler = (event) => {
 export const dropHandler = (event) => {
   event.stopPropagation();
   event.preventDefault();
+
+  if (event.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < event.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (event.dataTransfer.items[i].kind === 'file') {
+        var file = event.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < event.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
+    }
+  }
+
   hideDropZone(event);
   console.log("File dropped");
 
