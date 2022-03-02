7151340739a3ba298759e9c1e87b0a48968d5d08
Marlin <blutuu1@gmail.com>
2022-03-02 09:57:48 -0500

    minor change

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { IconContext } from "react-icons";
 import { BiUpload } from "react-icons/bi";
 
-  const SlideItem = styled.div`
-    background-repeat: no-repeat;
-    background-size: contain;
-    background-position: center;
-  `;
+const SlideItem = styled.div`
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+`;
 
 const Slide = ({ imageFile }) => {
-  return (
-    <SlideItem className="slide ba bg-washed-blue" style={{backgroundImage: `url(${imageFile.url})`}}>
+  const [imageName, setImageName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    if (imageFile.name) {
+      setImageName(imageFile.name);
+      setImageUrl(imageFile.url);
+    }
+    console.log(imageFile.name);
+  }, [imageFile]);
+
+  return !imageFile ? (
+    <h5>Loading...</h5>
+  ) : (
+    <SlideItem
+      className="slide ba bg-washed-blue"
+      style={{ backgroundImage: `url(${imageFile.url})` }}
+    >
       <IconContext.Provider
         value={{
           size: "1.5rem",
