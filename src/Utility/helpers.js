28e22cc48c35d5dce110527d117f040c8c0635f0
Marlin <blutuu1@gmail.com>
2023-07-24 18:02:53 -0400

    Prepped dropped files for upload
    Converted raw image data to blobs which can then be sent via POST.

--- src/Utility/helpers.js
+++ src/Utility/helpers.js
@@ -20,19 +20,20 @@ export const uploadFiles = (files) => {
   let url = "";
   let formData = new FormData();
 
-  for (const file in files) {
-    formData.append("file", file.url, file.name);
-    console.log(file);
+  for (const file of files) {
+    formData.append("image_file", dataURItoBlob(file.url), file.name);
   }
 
   for (const pair of formData.entries()) {
     console.log(pair);
   }
 
-  // fetch(url, {
-  //   method: "POST",
-  //   body: formData,
-  // });
+  console.log(formData.getAll("image_file"));
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+  });
 };
 
 export const getSlidePosition = (slide_element) => {
@@ -85,3 +86,22 @@ export const isDeleteClick = (element) => {
 
   return false;
 };
+
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(",")[0].indexOf("base64") >= 0)
+    byteString = atob(dataURI.split(",")[1]);
+  else byteString = unescape(dataURI.split(",")[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], { type: mimeString });
+}
